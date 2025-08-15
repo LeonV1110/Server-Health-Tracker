7616a8fea842e55af6d3ef2112f3834864ff47a1
LeonV1110 <59734771+LeonV1110@users.noreply.github.com>
2025-08-15 12:35:29 +0200

    added logging and error handling for servers that are turned off

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Server Health Tracker
 
+Will run of the datatime on the local system --> recommended to be installed on the same machine as your servers
--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 """DOCSTRING"""#TODO
 import configparser
 import time
+import logging
+from logging.handlers import TimedRotatingFileHandler
+import sys
 import datetime
 import requests
 import pymysql
 
-
+# Import configs
 config = configparser.ConfigParser()
 config.read('server-health-tracker/config.ini')
 
@@ -23,6 +26,27 @@ INTERVAL = int(config['SETTINGS']['INTERVAL'])
 SERVERIDS = config['SERVERID']
 SERVERNUMBERS = config['SERVER_NUMBER']
 
+# Setup logging
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+file_handler = TimedRotatingFileHandler(
+    "server-health-tracker/logs/app.log",           # Base log file name
+    when="midnight",                                # Rotate at midnight
+    interval=1,                                     # Every 1 day
+    backupCount=7,                                  # Keep last 7 days
+    encoding="utf-8"                                # Avoid encoding issues
+)
+file_handler.setLevel(logging.ERROR)
+
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(logging.INFO)
+
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+
+
 
 if len(SERVERIDS) != len(SERVERNUMBERS):
     raise ValueError
@@ -36,32 +60,59 @@ headers = {
     "Accept": "application/vnd.wisp.v1+json",
     "Authorization": f"Bearer {API_KEY}"
 }
-
-connection = pymysql.connect(host=DATABASEHOST, port=int(DATABASEPORT),
+def connectDB():
+    return pymysql.connect(host=DATABASEHOST, port=int(DATABASEPORT),
                              user = DATABASEUSER, password=DATABASEPSW, database=DATABASENAME)
 
 
-
-while True:
-    for server_num, server_url in servers.items():
-        try:
-            response = requests.get(server_url, headers=headers, timeout=max(5, INTERVAL/5))
-            response.raise_for_status()
-            data = response.json()
-            memory = data['proc']['memory']['total']
-            print(datetime.datetime.now())
-            print('memory:', memory)
-            cpu = data['proc']['cpu']['total']
-            print('cpu:', cpu)
-            SQL = "INSERT INTO `stats` (`time`, `server_id`, `cpu`, `memory`) VALUES (%s, %s, %s, %s)" #TODO
-            VALUES = (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), server_num, cpu, memory)
-            with connection.cursor() as cursor:
-                cursor.execute(SQL, VALUES)
-            connection.commit()
+def main():
+    connection = None
+    while True:
+        #reconnect to DB if needed
+        try: 
+            if connection is None or not connection.open:
+                logger.warning("Lost database connection. Reconnecting...")
+                connection = connectDB()
+                logger.info("Database reconnected.")
         except pymysql.OperationalError as e:
-            print('Database operation failed:', e)
+            logger.error(f'Reconnecting failed: {e}', exc_info=True)
+        
+        for server_num, server_url in servers.items():
+            try:
+                logger.info(f'Polling server {server_num}')
+                response = requests.get(server_url, headers=headers, timeout=max(5, INTERVAL/5))
+                response.raise_for_status()
 
-        except requests.RequestException as e:
-            print("Request failed:", e)
+                data = response.json()
+                logger.debug('_____________________')
+                logger.debug(data)
+                logger.debug('_____________________')
+                if data['status'] != 0:
+                    memory = data['proc']['memory']['total']
+                    cpu = data['proc']['cpu']['total']
+                    logger.info(datetime.datetime.now())
+                    logger.info(f'memory: {memory}')
+                    logger.info(f'cpu: {cpu}')
 
-    time.sleep(INTERVAL)
+                    SQL = "INSERT INTO `stats` (`time`, `server_id`, `cpu`, `memory`) VALUES (%s, %s, %s, %s)" #TODO
+                    VALUES = (datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), server_num, cpu, memory)
+                    with connection.cursor() as cursor:
+                        cursor.execute(SQL, VALUES)
+                    connection.commit()
+                else:
+                    logger.info(f'Server {server_num} is not running')
+            except pymysql.OperationalError as e:
+                logger.error(f'Database operation failed: {e}', exc_info=True)
+
+            except requests.RequestException as e:
+                logger.error(f"Request failed: {e}", exc_info=True)
+            
+        logger.info(f'Waiting {INTERVAL}s')
+        time.sleep(INTERVAL)
+
+
+if __name__ == "__main__":
+    try: 
+        main()
+    except Exception as e:
+        logger.error(f"Catastrofic error: {e}", exc_info=True)
